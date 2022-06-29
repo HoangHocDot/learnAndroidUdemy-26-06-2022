577f6a40c2bf54b903dca297fce3e97118e90b86
HoangNV2425 <hoang.nv.2425@gmail.com>
2022-06-29 23:16:16 +0700

    c1 - commit1

---
+++ app/src/main/java/com/maclaurin/nvh/convertcurrency/MainActivity.java
@@ -0,0 +1,26 @@
+package com.maclaurin.nvh.convertcurrency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    
+    public void convertUsd(View view) {
+        EditText editText = findViewById(R.id.editTextNumberDecimal);
+        String amountInPounds = editText.getText().toString();
+        String amountInUsd = String.format("%.2f", Double.parseDouble(amountInPounds) * 1.3)  ;
+        Toast.makeText(this, amountInPounds +"£ = " + amountInUsd +"$", Toast.LENGTH_SHORT).show();
+        Toast.
+    }
+
+        
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
